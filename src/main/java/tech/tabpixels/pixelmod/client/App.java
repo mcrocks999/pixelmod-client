6f418fecaafa2ea680efceb940ff76f239f1982c
mcrocks999 <paulmakles@gmail.com>
2016-12-17 11:06:56 +0000

    Setup

---
+++ src/main/java/tech/tabpixels/pixelmod/client/App.java
@@ -0,0 +1,13 @@
+package tech.tabpixels.pixelmod.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
